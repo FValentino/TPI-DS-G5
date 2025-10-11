6f7ae98d0ae4bdff851c6770af81ae5c3fe600a9
unknown <fariasvalentino25@gmail.com>
2025-10-11 12:50:18 -0300

    Initial config

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,35 @@
 import express, { json } from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173'
+];
+
+/*
+  Configuracion para orgigenes y permisos 
+  (Acepta cualquier peticion que venga las urls establecidas y con el header de credenciales)
+*/
+const corsOptions: cors.CorsOptions = {
+  // Solo permite las URLs definidas en el arreglo allowedOrigins
+  origin: allowedOrigins,
+  
+  // Es vital permitir credenciales si usas headers de Autorización (JWT)
+  //credentials: true, 
+};
+
 app.use(json());
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Prueba api node'))
 
-app.listen(8080, ()=>{
+app.listen(PORT, ()=>{
   console.log("Api funcionando en el puerto 8080")
 }) 
--- src/config/appDataSource.ts
+++ src/config/appDataSource.ts
@@ -7,18 +7,27 @@ dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
+  type: "postgres", // <-- ¡CAMBIO CLAVE AQUÍ!
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432", 10),
+  port: parseInt(process.env.DB_PORT || "5432", 10), // Ahora el default es correcto
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false, 
   },
+  /* 
+    La linea de synchronize se usa para sincronizar la base de datos de forma automatica
+    cada vez que se haga un cambio en el sistema.
+
+    true -> Se sincroniza automaticamente
+    false -> Los cambios se hacen a mano
+
+    Decidan ustedes cual configuracion usar
+  */ 
   synchronize: true, 
   logging: true,
-  entities: [Product],
+  entities: [], //<-- Aca van las entidades o modelos
   migrations: ["src/migrations/*.ts"],
   subscribers: [],
 });
